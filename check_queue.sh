080d0b861545749f9873d6abea3d70f68c8e1945
andrze <chlebickiandrzej@gmail.com>
2020-10-12 11:43:15 +0200

    Dodano skrypt do sprawdzania kolejki zadań na klastrze

---
+++ check_queue.sh
@@ -0,0 +1,16 @@
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+
+catalog='/home/andrzej/Documents/Studia/Anizotropie/FunctionalRenormalization/'
+nq=$catalog.new_queue
+oq=$catalog.old_queue
+
+queue=$(ssh ac357729@kruk-host.fuw.edu.pl 'python ~/Documents/get_qstat.py') || return
+
+echo "$queue" | head -n 3 > $nq
+
+cmp --silent $oq $nq || notify-send "Zmiana w kolejce" "$(cat $nq)"
+
+mv $nq $oq
+
+
